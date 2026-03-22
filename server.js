79a17f16bde508658b9d37ebe5efc929457a5568
sharatkatwa <sjkatwa2000@gmail.com>
2026-03-22 12:45:37 +0530

    added env.YT_COOKIES

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const ytdlp_path = fs.existsSync("/opt/render/.local/bin/yt-dlp") ? "/opt/render
 const os = require("os");
 
 const cookiesPath = path.join(os.tmpdir(), "cookies.txt");
+fs.writeFileSync(cookiesPath, process.env.YT_COOKIES);
 /**
  * 📌 Get available formats (clean JSON)
  */
